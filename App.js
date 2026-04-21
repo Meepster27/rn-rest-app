4d95c15095eaf2700f619f2d256294de8534ba59
Meepster27 <sharpieisme27@gmail.com>
2026-04-21 11:38:35 -0500

    app.js

---
+++ App.js
@@ -0,0 +1,5 @@
+import { registerRootComponent } from 'expo';
+import App from './src/App';
+
+// registerRootComponent ensures App works in Expo, Snack, and native builds
+registerRootComponent(App);
