ad755b3c6e3e07147ba676f1913b67d2744d5b09
Meepster27 <sharpieisme27@gmail.com>
2026-04-21 11:47:56 -0500

    app

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { registerRootComponent } from 'expo';
 import App from './src/App';
 registerRootComponent(App);
